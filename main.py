bc75549dfca76ef6e6b7259c0ea6351352f8bf18
gsoghoyan <70809936+gsoghoyan@users.noreply.github.com>
2020-09-08 18:24:39 +0200

    http and https checked on develop branch

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def isurl(my_url):
 
     if my_url[0:4] == "http":
         print(my_url, " is valid URL")
+    elif my_url[0:5] == "https":
+        print(my_url, " is valid URL")
     else: 
         print(my_url, " is not valid URL")
 
