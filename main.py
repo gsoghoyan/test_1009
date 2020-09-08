b25c2d9df8cdd067b189be5affd018e3954c178a
gsoghoyan <70809936+gsoghoyan@users.noreply.github.com>
2020-09-08 18:22:15 +0200

    http checked

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ def isurl(my_url):
 
     if my_url[0:4] == "http":
         print(my_url, " is valid URL")
-    elif my_url[0:5] == "https":
-        print(my_url, " is valid URL")
     else: 
         print(my_url, " is not valid URL")
 
